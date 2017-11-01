646afc9c37365d939e2b21e96347e0df544d6a5c
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-11-02 00:31:59 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AlarmSurvivalTest
+{
+
+    using Android.App;
+    using Android.Widget;
+    using Android.OS;
+    using Android.Content;
+    using System;
+
+    [Activity(Label = "AlarmSurvivalTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private object timePicker;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            TimePicker timePicker = FindViewById<TimePicker>(Resource.Id.timePicker);
+            Button timePickerDone = FindViewById<Button>(Resource.Id.timePickerDone);
+
+            timePicker.SetIs24HourView(Java.Lang.Boolean.True);
+            timePicker.Hour = 24;
+            timePicker.Minute = 0;
+
+            timePickerDone.Click += (object sender, EventArgs e) =>
+            {
+                string date = DateTime.Today.ToShortDateString();
+                string hh = timePicker.Hour.ToString("D2");
+                string mm = timePicker.Minute.ToString("D2");
+                string dateTimeString = date +" "+ hh +":"+ mm +":"+"00.00";
+                DateTime dateTime = Convert.ToDateTime(dateTimeString);
+                TimeSpan span = dateTime - DateTime.Now;
+                string spanString = span.ToString();
+                long schedule = (long)(Java.Lang.JavaSystem.CurrentTimeMillis() + span.TotalMilliseconds);
+
+                Intent intent = new Intent(this, typeof(MyTestReceiver));
+                PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, intent, PendingIntentFlags.CancelCurrent);
+                AlarmManager alarmManager = (AlarmManager)this.GetSystemService(Context.AlarmService);
+                alarmManager.Set(AlarmType.RtcWakeup, schedule, pendingIntent);
+            };
+           
+        }
+
+    }
+
+    [BroadcastReceiver(Enabled = true)]
+    //no intent filter needed here
+    public class MyTestReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast toast = Toast.MakeText(Application.Context, "Hello", ToastLength.Long);
+            toast.Show();
+        }
+    }
+
+}
